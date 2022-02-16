8d5dc9175cfc19cde7d3bf96b3df93d5e46eb7bf
Ivan Fougerat <57118026+IvanFou@users.noreply.github.com>
2022-02-16 14:12:41 -0600

    Subiendo la app

---
+++ app/src/main/java/com/example/myapplication/BlankFragment.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class BlankFragment: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+        val root:View = inflater.inflate(R.layout.fragment_blank, container, false)
+        setupNavigation(root)
+        return root
+    }
+    private fun setupNavigation(root:View){
+        val detailButton:Button = root.findViewById<Button>(R.id.detail_button)
+        val createButton:Button = root.findViewById<Button>(R.id.create_button)
+
+        detailButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_list_to_detail)
+        }
+        createButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.create_list)
+        }
+    }
+}
